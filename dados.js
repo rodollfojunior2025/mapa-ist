915bd87d393767cd12164cdefc5b02906361031e
rodollfojunior2025 <jr.rodolfo.luis@gmail.com>
2026-02-23 20:09:55 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,16 +1,32 @@
-const dadosInsumos = [
-    { municipio: "AMAPÁ", lat: 2.0331, lon: -50.7957, masculino: 231264, feminino: 14200, gel: 0 },
-    { municipio: "CALÇOENE", lat: 2.4981, lon: -50.9469, masculino: 281364, feminino: 6600, gel: 0 },
-    { municipio: "CUTIAS", lat: 0.9844, lon: -50.8014, masculino: 104400, feminino: 11096, gel: 0 },
-    { municipio: "FERREIRA GOMES", lat: 0.8574, lon: -51.1848, masculino: 158720, feminino: 9600, gel: 8200 },
-    { municipio: "ITAUBAL", lat: 0.5945, lon: -50.6835, masculino: 168459, feminino: 10500, gel: 0 },
-    { municipio: "LARANJAL DO JARI", lat: -0.8425, lon: -52.5120, masculino: 457336, feminino: 20088, gel: 10500 },
-    { municipio: "MACAPÁ", lat: 0.0347, lon: -51.0664, masculino: 400000, feminino: 30000, gel: 20000 },
-    { municipio: "MAZAGÃO", lat: -0.1150, lon: -51.2894, masculino: 465300, feminino: 14300, gel: 0 },
-    { municipio: "OIAPOQUE", lat: 3.8414, lon: -51.8351, masculino: 443635, feminino: 36339, gel: 12000 },
-    { municipio: "PEDRA BRANCA", lat: 0.7758, lon: -51.9525, masculino: 230400, feminino: 5800, gel: 0 },
-    { municipio: "PORTO GRANDE", lat: 0.7127, lon: -51.4131, masculino: 360576, feminino: 20050, gel: 13100 },
-    { municipio: "SANTANA", lat: -0.0583, lon: -51.1817, masculino: 490000, feminino: 21850, gel: 15000 },
-    { municipio: "TARTARUGALZINHO", lat: 1.5052, lon: -50.9069, masculino: 237600, feminino: 0, gel: 0 },
-    { municipio: "VITÓRIA DO JARI", lat: -0.7874, lon: -52.1114, masculino: 252726, feminino: 4400, gel: 0 }
+const dadosMunicipios = [
+    { "nome": "Porto Grande", "lat": 0.7117, "lon": -51.4138, "masculino": 360576, "feminino": 20050, "gel": 13100 },
+    { "nome": "Ferreira Gomes", "lat": 0.8554, "lon": -51.1821, "masculino": 158720, "feminino": 9600, "gel": 8200 },
+    { "nome": "Laranjal do Jari", "lat": -0.8400, "lon": -52.5218, "masculino": 457336, "feminino": 20088, "gel": 10500 },
+    { "nome": "Santana", "lat": -0.0432, "lon": -51.1645, "masculino": 490000, "feminino": 21850, "gel": 15000 },
+    { "nome": "Vitória do Jari", "lat": -0.9262, "lon": -52.4231, "masculino": 252726, "feminino": 4400, "gel": 0 },
+    { "nome": "Calçoene", "lat": 2.5354, "lon": -50.9487, "masculino": 281364, "feminino": 6600, "gel": 0 },
+    { "nome": "Cutias do Araguari", "lat": 0.9717, "lon": -50.8008, "masculino": 104400, "feminino": 11096, "gel": 0 },
+    { "nome": "Pracuúba", "lat": 1.7432, "lon": -50.7847, "masculino": 96744, "feminino": 4180, "gel": 0 },
+    { "nome": "Tartarugalzinho", "lat": 1.5064, "lon": -50.9037, "masculino": 237600, "feminino": 6600, "gel": 0 },
+    { "nome": "Amapá", "lat": 2.0531, "lon": -50.7957, "masculino": 231264, "feminino": 14200, "gel": 0 },
+    { "nome": "Oiapoque", "lat": 3.8414, "lon": -51.8351, "masculino": 443635, "feminino": 36339, "gel": 12000 },
+    { "nome": "Pedra Branca do Amapari", "lat": 0.7758, "lon": -51.9525, "masculino": 230400, "feminino": 5800, "gel": 0 },
+    { "nome": "Mazagão", "lat": -0.1150, "lon": -51.2894, "masculino": 465300, "feminino": 14300, "gel": 0 },
+    { "nome": "Serra do Navio", "lat": 0.8900, "lon": -52.0000, "masculino": 84404, "feminino": 5990, "gel": 0 },
+    { "nome": "Itaubal", "lat": 0.5945, "lon": -50.6835, "masculino": 168459, "feminino": 10500, "gel": 0 },
+    { 
+      "nome": "Macapá", "lat": 0.0347, "lon": -51.0664, 
+      "masculino": 2189600, "feminino": 184669, "gel": 261200,
+      "masc_instituicoes": 1410380, "fem_instituicoes": 169801, "gel_instituicoes": 77871
+    }
+];
+
+const dadosInstituicoes = [
+    { "nome": "SAE/CTA Estadual (Centro de Testagem e Aconselhamento)", "masculino": 360000, "feminino": 48200, "gel": 16000 },
+    { "nome": "Maternidade de Alto Risco", "masculino": 0, "feminino": 3456, "gel": 0 },
+    { "nome": "SVS/AP (Superintendência de Vigilância em Saúde)", "masculino": 648464, "feminino": 115011, "gel": 53371 },
+    { "nome": "DSEI (Distrito Sanitário Especial Indígena)", "masculino": 50400, "feminino": 600, "gel": 0 },
+    { "nome": "IAPEN (Instituto de Administração Penitenciária)", "masculino": 29664, "feminino": 0, "gel": 0 },
+    { "nome": "HEMOAP (Hemocentro do Amapá)", "masculino": 3600, "feminino": 0, "gel": 0 },
+    { "nome": "CRDT (Centro de Referência em Doenças Tropicais)", "masculino": 318252, "feminino": 2534, "gel": 8500 }
 ];
