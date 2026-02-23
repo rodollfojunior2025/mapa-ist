351c2207a3dc652c88813f463ea00333c8c59547
rodollfojunior2025 <jr.rodolfo.luis@gmail.com>
2026-02-23 19:37:48 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,1 +1,16 @@
-
+const dadosInsumos = [
+    { municipio: "AMAPÁ", lat: 2.0331, lon: -50.7957, masculino: 231264, feminino: 14200, gel: 0 },
+    { municipio: "CALÇOENE", lat: 2.4981, lon: -50.9469, masculino: 281364, feminino: 6600, gel: 0 },
+    { municipio: "CUTIAS", lat: 0.9844, lon: -50.8014, masculino: 104400, feminino: 11096, gel: 0 },
+    { municipio: "FERREIRA GOMES", lat: 0.8574, lon: -51.1848, masculino: 158720, feminino: 9600, gel: 8200 },
+    { municipio: "ITAUBAL", lat: 0.5945, lon: -50.6835, masculino: 168459, feminino: 10500, gel: 0 },
+    { municipio: "LARANJAL DO JARI", lat: -0.8425, lon: -52.5120, masculino: 457336, feminino: 20088, gel: 10500 },
+    { municipio: "MACAPÁ", lat: 0.0347, lon: -51.0664, masculino: 400000, feminino: 30000, gel: 20000 },
+    { municipio: "MAZAGÃO", lat: -0.1150, lon: -51.2894, masculino: 465300, feminino: 14300, gel: 0 },
+    { municipio: "OIAPOQUE", lat: 3.8414, lon: -51.8351, masculino: 443635, feminino: 36339, gel: 12000 },
+    { municipio: "PEDRA BRANCA", lat: 0.7758, lon: -51.9525, masculino: 230400, feminino: 5800, gel: 0 },
+    { municipio: "PORTO GRANDE", lat: 0.7127, lon: -51.4131, masculino: 360576, feminino: 20050, gel: 13100 },
+    { municipio: "SANTANA", lat: -0.0583, lon: -51.1817, masculino: 490000, feminino: 21850, gel: 15000 },
+    { municipio: "TARTARUGALZINHO", lat: 1.5052, lon: -50.9069, masculino: 237600, feminino: 0, gel: 0 },
+    { municipio: "VITÓRIA DO JARI", lat: -0.7874, lon: -52.1114, masculino: 252726, feminino: 4400, gel: 0 }
+];
